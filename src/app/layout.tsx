e5233d9524d28ad25cc1869d5b1d5ed26c420078
altie122 <altielynn@gmail.com>
2025-04-04 15:09:18 -0600

    run biomw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,34 +7,34 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "BCUW | The BlossomCraft Unofficial Wiki",
-  description:
-    "The BlossomCraft Unofficial Wiki | The community-run BlossomCraft Wiki, documenting since August 13, 2023.",
-  icons: [{ rel: "icon", url: "/bcuwOnlyTitleSquare.png" }],
+	title: "BCUW | The BlossomCraft Unofficial Wiki",
+	description:
+		"The BlossomCraft Unofficial Wiki | The community-run BlossomCraft Wiki, documenting since August 13, 2023.",
+	icons: [{ rel: "icon", url: "/bcuwOnlyTitleSquare.png" }],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang='en' suppressHydrationWarning>
-      <body className='h-svh w-screen overflow-hidden'>
-        {" "}
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          <SidebarProvider>
-            <AppSidebar />
-            <main className='flex flex-col w-full h-full'>
-              <Navbar />
-              <div className='h-[calc(100vh-3rem)]'>{children}</div>
-            </main>
-          </SidebarProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className="h-svh w-screen overflow-hidden">
+				{" "}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<SidebarProvider>
+						<AppSidebar />
+						<main className="flex h-full w-full flex-col">
+							<Navbar />
+							<div className="h-[calc(100vh-3rem)]">{children}</div>
+						</main>
+					</SidebarProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
