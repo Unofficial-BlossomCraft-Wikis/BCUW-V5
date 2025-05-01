3b64dc5d901fc1dc2ab7fb04a737bfd440555119
altie122 <altielynn@gmail.com>
2025-04-30 21:20:49 -0600

    transfer old files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,51 @@
+import { TitleDataUpdater } from "@/components/title-provider";
 import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
-
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
-  return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
-        </div>
-      </main>
-    </HydrateClient>
-  );
+export default function HomePage() {
+	return (
+		<>
+			<TitleDataUpdater data={{ title: "Main Page" }} />
+			<main className="p-2">
+				<div className="flex h-1/2 flex-col-reverse justify-center md:flex-row">
+					<div className="prose flex flex-col items-center justify-center text-center md:basis-1/2 ">
+						<h1>BCUW</h1>
+						<p>
+							BlossomCraft Unofficial Wiki
+							<br />
+							<>
+								Owned and maintained by{" "}
+								<span className="mention">@🌸 Altie122</span>
+							</>
+						</p>
+						<p>
+							{/* Now with a total of{" "}
+              <span className='text-ranks-celestial'>{allItems.length}</span> items
+              and <span className='text-ranks-celestial'>{allCrates.length}</span>{" "}
+              crates! (and we are always adding more) */}
+							We are currently working on rewriting the entire site from scratch
+							for V5. We are working on getting pages and page counts for crates
+							and items up, sorry for the inconvenience. please check{" "}
+							<Link href="https://hub.bcuw.xyz">V3 or V4</Link> for complete
+							information.
+						</p>
+					</div>
+					<div className="flex items-center justify-center md:basis-1/2 ">
+						<img
+							src="/bcuwTitleNormal.png"
+							alt="BCUW"
+							className="h-auto w-1/2"
+							style={{
+								filter: "drop-shadow(0 0 3rem hsla(306, 66%, 66%, 0.2))",
+							}}
+							width={400}
+							height={400}
+						/>
+					</div>
+				</div>
+        <p>
+          
+        </p>
+			</main>
+		</>
+	);
 }
